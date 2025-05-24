55e72a82a00d8d5fdcd59cbb170237b0b5e8c13c
IsmaBamo <missisma6@gmail.com>
2025-05-24 17:44:51 +0530

    commit

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ require('dotenv').config();
 const schoolRoutes = require('./routes/schoolRoutes');
 
 app.use(express.json());
-app.use('/', schoolRoutes);
+
+app.get('/', (req, res) => {
+  res.send('School API is running!');
+});
+
+
+app.use('/api', schoolRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
